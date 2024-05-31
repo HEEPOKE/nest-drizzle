7809e6645cc2002b45bfc3b075fad266e6070931
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2024-05-31 10:22:56 +0700

    build: set config :money_with_wings:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { globalConfig } from './configs/configs';
+import { ApisModule } from './apis/apis.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: '.env',
+      load: [globalConfig],
+    }),
+    ApisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
